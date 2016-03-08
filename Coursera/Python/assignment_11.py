5bed01a96d02c6fe6f92adc4c2ebe79da4a5e157
HungChe Lo <hclo@snese.net>
2016-03-09 00:17:35 +0800

    shorter version of assignment 11

--- Coursera/Python/assignment_11.py
+++ Coursera/Python/assignment_11.py
@@ -1,11 +1,2 @@
 import re
-handle = open("regex_sum_245522.txt", "r")
-
-result = []
-
-for line in handle:
-    tmp = re.findall("[0-9]+", line)
-    if tmp:
-        result += tmp
-
-print sum(int(num) for num in result)
+print sum(int(i) for i in re.findall("[0-9]+", open("regex_sum_245522.txt", "r").read()))
