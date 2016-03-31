960016c1c8b6747436c389c5839801d41203d417
Hung Che Lo <hclo@snese.net>
2016-03-31 16:34:24 +0800

    revise for assignment_12.1 request

--- Coursera/Python/assignment_12.1.py
+++ Coursera/Python/assignment_12.1.py
@@ -1,6 +1,15 @@
 # Note - this code must run in Python 2.x and you must download
 # http://www.pythonlearn.com/code/BeautifulSoup.py
 # Into the same folder as this program
+#
+# data
+# Sample data: http://python-data.dr-chuck.net/comments_42.html (Sum=2553)
+# Actual data: http://python-data.dr-chuck.net/comments_245527.html (Sum ends with 32)
+#
+# data format
+# <tr><td>Modu</td><td><span class="comments">90</span></td></tr>
+# <tr><td>Kenzie</td><td><span class="comments">88</span></td></tr>
+# <tr><td>Hubert</td><td><span class="comments">87</span></td></tr>
 
 import urllib
 from BeautifulSoup import *
@@ -10,11 +19,15 @@ html = urllib.urlopen(url).read()
 
 soup = BeautifulSoup(html)
 
+count = 0
+sum = 0
+
 # Retrieve all of the anchor tags
 tags = soup('span')
 for tag in tags:
 	# Look at the parts of a tag
-	print 'TAG:',tag
-	print 'URL:',tag.get('href', None)
-	print 'Contents:',tag.contents[0]
-	print 'Attrs:',tag.attrs
+	count += 1
+	sum += int(tag.contents[0])
+
+print 'Count',count
+print 'Sum',sum
