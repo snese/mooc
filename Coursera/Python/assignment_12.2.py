ab225f4fbbb6ee5c32999867ecdc2170298ce91b
Hung Che Lo <hclo@snese.net>
2016-04-01 10:11:16 +0800

    base on urllink.py that implement the assignment 12.2 and add re to show last name

--- Coursera/Python/assignment_12.2.py
+++ Coursera/Python/assignment_12.2.py
@@ -1,15 +1,28 @@
 # Note - this code must run in Python 2.x and you must download
 # http://www.pythonlearn.com/code/BeautifulSoup.py
 # Into the same folder as this program
+# Sample problem: Start at http://python-data.dr-chuck.net/known_by_Fikret.html
+# Actual problem: Start at: http://python-data.dr-chuck.net/known_by_Kieryn.html
 
+import re
 import urllib
 from BeautifulSoup import *
 
-url = raw_input('Enter - ')
+url = raw_input('Enter URL: ')
 html = urllib.urlopen(url).read()
 soup = BeautifulSoup(html)
 
-# Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print tag.get('href', None)
+position = int(raw_input('Enter position: '))
+count = int(raw_input('Enter count: '))
+
+print "Retrieving:",url
+
+while count > 0:
+	tags = soup('a')
+	print "Retrieving:",tags[position - 1].get('href', None)
+	url = tags[position - 1].get('href', None)
+	html = urllib.urlopen(url).read()
+	soup = BeautifulSoup(html)
+	count -= 1
+
+print "Last Name:",re.findall(r"[A-Z][a-z]+",url)[0]
