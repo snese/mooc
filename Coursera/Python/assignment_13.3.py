d3683b0fba1e928b1efd95b96a3d84bfd1e1d552
Hung Che Lo <hclo@snese.net>
2016-04-07 16:27:45 +0800

    complete the assignment 13.3

--- Coursera/Python/assignment_13.3.py
+++ Coursera/Python/assignment_13.3.py
@@ -1,8 +1,18 @@
+# Test Data / Sample Execution
+# $ python solution.py
+# Enter location: South Federal University 
+# Retrieving http://...
+# Retrieved 2101 characters
+# Place id ChIJJ8oO7_B_bIcR2AlhC8nKlok 
+#
+# assignment
+# Lisandro Alvarado
+
 import urllib
 import json
 
-# serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
-serviceurl = 'http://python-data.dr-chuck.net/geojson?'
+serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+# serviceurl = 'http://python-data.dr-chuck.net/geojson?'
 
 while True:
     address = raw_input('Enter location: ')
@@ -21,11 +31,5 @@ while True:
         print data
         continue
 
-    print json.dumps(js, indent=4)
-
-    lat = js["results"][0]["geometry"]["location"]["lat"]
-    lng = js["results"][0]["geometry"]["location"]["lng"]
-    print 'lat',lat,'lng',lng
-    location = js['results'][0]['formatted_address']
-    print location
+    print 'place_id',js['results'][0]['place_id']
 
