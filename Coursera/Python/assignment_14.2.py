e7c0df10be64df6413f89b651666dc3d39d30938
Hung Che Lo <hclo@snese.net>
2016-04-19 17:16:22 +0800

    revise table name from email to org

--- Coursera/Python/assignment_14.2.py
+++ Coursera/Python/assignment_14.2.py
@@ -1,15 +1,17 @@
 import sqlite3
+import timeit
 
-conn = sqlite3.connect('emaildb.sqlite')
+conn = sqlite3.connect('assignment_14.2.sqlite')
 cur = conn.cursor()
 
 cur.execute('''
 DROP TABLE IF EXISTS Counts''')
 
 cur.execute('''
-CREATE TABLE Counts (email TEXT, count INTEGER)''')
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
 fname = raw_input('Enter file name: ')
+timer_start = timeit.default_timer()
 if ( len(fname) < 1 ) : fname = 'mbox-short.txt'
 fh = open(fname)
 for line in fh:
@@ -17,21 +19,22 @@ for line in fh:
     pieces = line.split()
     email = pieces[1]
     print email
-    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email, ))
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (email, ))
     row = cur.fetchone()
     if row is None:
-        cur.execute('''INSERT INTO Counts (email, count) 
+        cur.execute('''INSERT INTO Counts (org, count) 
                 VALUES ( ?, 1 )''', ( email, ) )
     else : 
-        cur.execute('UPDATE Counts SET count=count+1 WHERE email = ?', 
+        cur.execute('UPDATE Counts SET count=count+1 WHERE org = ?', 
             (email, ))
     # This statement commits outstanding changes to disk each 
     # time through the loop - the program can be made faster 
     # by moving the commit so it runs only after the loop completes
-    conn.commit()
+
+conn.commit()
 
 # https://www.sqlite.org/lang_select.html
-sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
 print
 print "Counts:"
@@ -39,4 +42,8 @@ for row in cur.execute(sqlstr) :
     print str(row[0]), row[1]
 
 cur.close()
+
+timer_end = timeit.default_timer()
+
+print "----",timer_end - timer_start
 
